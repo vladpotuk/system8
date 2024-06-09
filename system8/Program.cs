39b69820f0825d5e3e9b799c85813cf0784a2c3d
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-09 13:48:13 +0300

    update

--- system8/Program.cs
+++ system8/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+using System.Threading;
+
+namespace system7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isNewInstance;
+            string mutexName = "SingleInstanceAppMutex";
+
+            using (Mutex mutex = new Mutex(true, mutexName, out isNewInstance))
+            {
+                if (isNewInstance)
+                {
+                    Console.WriteLine("Додаток запущено. Натисніть будь-яку клавішу для виходу.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Додаток вже запущено. Друга копія не може бути запущена.");
+                }
+            }
+        }
+    }
+}
+
